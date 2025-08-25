ef9718d9552fc2906f084892b6c7cfde627dc3af
Sabbir486 <australiasabbir355@gmail.com>
2025-08-25 22:49:55 +0600

    Parameter

--- Milestone- 4/01_Functions/02_Parameter.js
+++ Milestone- 4/01_Functions/02_Parameter.js
@@ -18,3 +18,23 @@ function square(val){
 
 square(5);
 square(12);
+console.log('----------');
+
+const number = 25;
+square(number);
+console.log('----------');
+
+
+
+// Subtraction
+
+function subtract(num1, num2) {
+    const diff = (num1-num2);
+    console.log(diff);
+    
+    
+}
+
+const age1 = 10;
+const age2 = 5;
+subtract(age1 , age2);
