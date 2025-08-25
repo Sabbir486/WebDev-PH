4dec22ac8e25437a80ecdcefd5b7ce692ae72f70
Sabbir486 <australiasabbir355@gmail.com>
2025-08-25 23:24:23 +0600

    Value Return

---
+++ Milestone- 4/01_Functions/03_Return.js
@@ -0,0 +1,10 @@
+function tenTime (num){
+    const result = num*10;
+    return result
+}
+
+let val1 = tenTime(5);
+let val2 = 100;
+
+const output = val1 + val2;
+console.log("Total Output: ", output);
