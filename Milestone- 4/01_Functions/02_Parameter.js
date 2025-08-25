02d17786068c3eb0175f2ff0d76176660a804e1f
Sabbir486 <australiasabbir355@gmail.com>
2025-08-25 22:37:03 +0600

    Function Parameter

---
+++ Milestone- 4/01_Functions/02_Parameter.js
@@ -0,0 +1,20 @@
+// function vatKha() {
+//     console.log("Hat duo");
+//     console.log("Table a bosho");
+//     console.log("Vat Khao");
+    
+// }
+
+// // Function call
+
+// vatKha();
+
+function square(val){
+    let value = val * val;
+    console.log("Square Value: ", value);
+    // return value;
+    
+}
+
+square(5);
+square(12);
