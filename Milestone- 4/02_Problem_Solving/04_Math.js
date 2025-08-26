5672a01def81fca2aeaf3edcc91e97995d2eb05a
Sabbir486 <australiasabbir355@gmail.com>
2025-08-27 03:19:20 +0600

    Math Operations

---
+++ Milestone- 4/02_Problem_Solving/04_Math.js
@@ -0,0 +1,30 @@
+// Math is an Object-->
+
+// const min = Math.min;
+
+const min = Math.min(1,4,8,7,3,5,2);
+console.log(min);
+console.log("---------------");
+
+console.log(Math.PI);
+console.log("---------------");
+
+console.log(Math.abs(-105)); //Always Positive Value Provide
+console.log("---------------");
+
+console.log(Math.round(10.85));
+console.log("---------------");
+
+console.log(Math.floor(10.857));
+console.log("---------------");
+
+console.log(Math.ceil(10.2));
+console.log("---------------");
+
+console.log(Math.round(Math.random()*10)); //Give Integer Value
+
+
+
+
+
+
