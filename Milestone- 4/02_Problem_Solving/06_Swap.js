dc6b08efdbe27ba4e50495961ed5e23a25b82fff
Sabbir486 <australiasabbir355@gmail.com>
2025-08-27 03:39:12 +0600

    Swap Function

---
+++ Milestone- 4/02_Problem_Solving/06_Swap.js
@@ -0,0 +1,28 @@
+// Swapping -->
+
+let a = 5;
+let b = 9;
+
+let c;
+c = a;
+// console.log(c);
+
+a = b;
+console.log(a);
+
+b = c;
+console.log(b);
+
+console.log("-----------------");
+
+let x = 15;
+let y = 20;
+console.log(x, y);
+
+[x , y] = [y , x];
+console.log(x, y);
+
+
+
+
+
