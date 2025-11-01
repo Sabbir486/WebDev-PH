f73d5608c62e05f345533a8d52e674c69de098b9
Sabbir486 <australiasabbir355@gmail.com>
2025-11-02 02:56:58 +0600

    Add

--- Milestone- 5/02_Second_Module/JavaScript/events.js
+++ Milestone- 5/02_Second_Module/JavaScript/events.js
@@ -1,1 +1,20 @@
 console.log('External File');
+
+// Option-2: Event Handler (Using Function)
+
+        function makeYellow() {
+            document.body.style.backgroundColor = 'yellow';
+        }
+
+        function makeRed() {
+            document.body.style.backgroundColor = 'red';
+        }
+
+// Option-3: Event Handler (Another Method)
+
+        const btnMakeBlue = document.getElementById('btn-make-blue')
+        // console.log(btnMakeBlue);
+        
+        btnMakeBlue.onclick = function makeBlue(){
+            document.body.style.backgroundColor = 'blue';
+        }
