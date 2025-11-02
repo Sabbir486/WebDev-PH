a6084e665910389aaea4e03ca054c897f0a43519
Sabbir486 <australiasabbir355@gmail.com>
2025-11-03 02:43:20 +0600

    Handler js

--- Milestone- 5/02_Second_Module/JavaScript/handler.js
+++ Milestone- 5/02_Second_Module/JavaScript/handler.js
@@ -1,1 +1,10 @@
 console.log("Handler file ADDED");
+
+
+// Option-1: Event Handler
+
+        document.getElementById('btn-update-title').addEventListener('click', function btnUpdate(){
+            const titleText = document.getElementById('title-text');
+            titleText.innerText = 'Updated Page Title Text';
+            
+        })
