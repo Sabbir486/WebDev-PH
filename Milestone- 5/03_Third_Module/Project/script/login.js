6c10c6b4db5264499ac61870271bfd78b850a854
Sabbir486 <australiasabbir355@gmail.com>
2025-12-08 04:27:42 +0600

    Add

--- Milestone- 5/03_Third_Module/Project/script/login.js
+++ Milestone- 5/03_Third_Module/Project/script/login.js
@@ -1,6 +1,31 @@
 // alert()
 
-document.getElementById("login-btn").addEventListener("click", function(){
+document.getElementById("login-btn").addEventListener("click", function(event){
+    event.preventDefault();
+    // console.log("Hello");
+
+    const accountNumber = document.getElementById('Account-number').value;
+    // console.log(accountNumber);
     
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(pinNumber);
     
+    if(accountNumber.length == 5){
+        if(parseInt(pinNumber) === 1234){  //parseInt => Covert String to a Int
+            console.log("Valid");
+        
+        }
+
+        else{
+            console.log("Not Valid Pin");
+            
+        }
+        
+    }
+
+    else{
+        console.log("Not Valid Account Number");
+        
+    }
+
 })
