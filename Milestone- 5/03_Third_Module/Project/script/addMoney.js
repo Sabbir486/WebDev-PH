f799069db6e143b4b1a999c1f063a18851efc79f
Sabbir486 <australiasabbir355@gmail.com>
2025-12-09 04:35:27 +0600

    Add Money

---
+++ Milestone- 5/03_Third_Module/Project/script/addMoney.js
@@ -0,0 +1,21 @@
+document.getElementById('add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    const amount = document.getElementById('amount').value;
+    const convertedAmount = parseInt(amount);
+    
+    const pin = document.getElementById('pin-number').value;
+
+    const mainBalance = document.getElementById('money-dollar').innerText;
+    const convertedBalance = parseFloat(mainBalance);
+
+    if(parseInt(pin) === 1234){
+        const sum = convertedAmount + convertedBalance;
+        document.getElementById('money-dollar').innerText = sum;
+        
+    }
+
+    else{
+        console.log("Not Possible Adding");
+        
+    }
+})
