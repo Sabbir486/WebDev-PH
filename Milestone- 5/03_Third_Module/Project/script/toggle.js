f15eb909f27ab65288d228066c4971ae0efdd9b2
Sabbir486 <australiasabbir355@gmail.com>
2025-12-10 02:48:24 +0600

    Toggle

--- Milestone- 5/03_Third_Module/Project/script/toggle.js
+++ Milestone- 5/03_Third_Module/Project/script/toggle.js
@@ -4,12 +4,20 @@ document.getElementById('cashout').style.display = "none"; //by-default
 
 document.getElementById('add-money-box').addEventListener('click',function() {
     document.getElementById('cashout').style.display = "none";
+    document.getElementById('paybill').style.display = "none";
     document.getElementById('addmoney').style.display = "block";
 
 })
 
 document.getElementById('cash-out-box').addEventListener('click',function() {
     document.getElementById('cashout').style.display = "block";
+    document.getElementById('paybill').style.display = "none";
     document.getElementById('addmoney').style.display = "none";
 
+})
+
+document.getElementById('paybill-box').addEventListener('click',function(){
+    document.getElementById('cashout').style.display = "none";
+    document.getElementById('addmoney').style.display = 'none';
+    document.getElementById('paybill').style.document = 'block';
 })
