6952807b9b77f401976aa6437156a2435ebbd6f0
Sabbir486 <australiasabbir355@gmail.com>
2025-12-12 23:21:41 +0600

    Add

--- Milestone- 5/03_Third_Module/Project/script/addMoney.js
+++ Milestone- 5/03_Third_Module/Project/script/addMoney.js
@@ -1,21 +1,30 @@
+// document.getElementById('add-money').addEventListener('click', function(event){
+//     event.preventDefault();
+//     const amount = document.getElementById('amount').value;
+//     const convertedAmount = parseInt(amount);
+    
+//     const pin = document.getElementById('pin-number').value;
+
+//     const mainBalance = document.getElementById('money-dollar').innerText;
+//     const convertedBalance = parseFloat(mainBalance);
+
+//     if(parseInt(pin) === 1234){
+//         const sum = convertedAmount + convertedBalance;
+//         document.getElementById('money-dollar').innerText  = sum;
+        
+//     }
+
+//     else{
+//         console.log("Not Possible Adding");
+        
+//     }
+// })
+
+
+
+// More Optimized Code
+
 document.getElementById('add-money').addEventListener('click', function(event){
     event.preventDefault();
-    const amount = document.getElementById('amount').value;
-    const convertedAmount = parseInt(amount);
-    
-    const pin = document.getElementById('pin-number').value;
 
-    const mainBalance = document.getElementById('money-dollar').innerText;
-    const convertedBalance = parseFloat(mainBalance);
-
-    if(parseInt(pin) === 1234){
-        const sum = convertedAmount + convertedBalance;
-        document.getElementById('money-dollar').innerText  = sum;
-        
-    }
-
-    else{
-        console.log("Not Possible Adding");
-        
-    }
 })
