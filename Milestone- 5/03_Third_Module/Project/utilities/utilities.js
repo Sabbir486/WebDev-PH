011879f3bb3467b88242d332c2bbf92b84f6dd7c
Sabbir486 <australiasabbir355@gmail.com>
2025-12-13 03:31:43 +0600

    Add

--- Milestone- 5/03_Third_Module/Project/utilities/utilities.js
+++ Milestone- 5/03_Third_Module/Project/utilities/utilities.js
@@ -1,6 +1,13 @@
-function getInputValueByID(){
-    // console.log("Hello Boos");
+function getInputValueByID(id){
+    const value = document.getElementById(id).value;
+    const convertValue = parseFloat(value);
+    return convertValue;
     
-    
+}
+
+function getInnerTextByID(id){
+    const value = document.getElementById(id).innerText;
+    const convertValue = parseFloat(value);
+    return convertValue;
 }
 
