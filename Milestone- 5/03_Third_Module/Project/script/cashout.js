68199daa09cb6acf9c8d09e489942c6a05a1a131
Sabbir486 <australiasabbir355@gmail.com>
2025-12-14 04:03:32 +0600

    Modified

--- Milestone- 5/03_Third_Module/Project/script/cashout.js
+++ Milestone- 5/03_Third_Module/Project/script/cashout.js
@@ -1,23 +1,23 @@
-document.getElementById('cashout-btn').addEventListener('click',function(event){
-    event.preventDefault();
+// document.getElementById('cashout-btn').addEventListener('click',function(event){
+//     event.preventDefault();
 
-    const pin = document.getElementById('pin-number-cashout').value;
-    const convertedPin = parseInt(pin);
+//     const pin = document.getElementById('pin-number-cashout').value;
+//     const convertedPin = parseInt(pin);
 
-    const amount = document.getElementById('cashout-amount').value;
-    const convertedAmount = parseFloat(amount);
+//     const amount = document.getElementById('cashout-amount').value;
+//     const convertedAmount = parseFloat(amount);
 
-    const balance = document.getElementById('money-dollar').innerText;
-    const convertedBalance = parseFloat(balance);
+//     const balance = document.getElementById('money-dollar').innerText;
+//     const convertedBalance = parseFloat(balance);
 
-    if(convertedPin === 1234){
-        const remainAmount = convertedBalance - convertedAmount;
-        document.getElementById('money-dollar').innerText = remainAmount;
-    }
+//     if(convertedPin === 1234){
+//         const remainAmount = convertedBalance - convertedAmount;
+//         document.getElementById('money-dollar').innerText = remainAmount;
+//     }
 
-    else{
-        console.log("Not Valid");
+//     else{
+//         console.log("Not Valid");
         
-    }
+//     }
 
-})
+// })
