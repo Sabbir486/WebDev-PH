a9cf3587cfbf196d747ea445deb4ed7c7d9ff520
Sabbir486 <australiasabbir355@gmail.com>
2025-12-14 03:44:05 +0600

    Add

--- Milestone- 5/03_Third_Module/Project/script/toggle.js
+++ Milestone- 5/03_Third_Module/Project/script/toggle.js
@@ -1,24 +1,24 @@
 // alert()
 
-document.getElementById('cashout').style.display = "none"; //by-default
-document.getElementById('paybill').style.display = "none"; //by-default
+// document.getElementById('cashout').style.display = "none"; //by-default
+// document.getElementById('paybill').style.display = "none"; //by-default
 
-document.getElementById('add-money-box').addEventListener('click',function() {
-    document.getElementById('cashout').style.display = "none";
-    document.getElementById('paybill').style.display = "none";
-    document.getElementById('addmoney').style.display = "block";
+// document.getElementById('add-money-box').addEventListener('click',function() {
+//     document.getElementById('cashout').style.display = "none";
+//     document.getElementById('paybill').style.display = "none";
+//     document.getElementById('addmoney').style.display = "block";
 
-})
+// })
 
-document.getElementById('cash-out-box').addEventListener('click',function() {
-    document.getElementById('cashout').style.display = "block";
-    document.getElementById('paybill').style.display = "none";
-    document.getElementById('addmoney').style.display = "none";
+// document.getElementById('cash-out-box').addEventListener('click',function() {
+//     document.getElementById('cashout').style.display = "block";
+//     document.getElementById('paybill').style.display = "none";
+//     document.getElementById('addmoney').style.display = "none";
 
-})
+// })
 
-document.getElementById('paybill-box').addEventListener('click',function(){
-    document.getElementById('cashout').style.display = "none";
-    document.getElementById('addmoney').style.display = 'none';
-    document.getElementById('paybill').style.document = 'block';
-})
+// document.getElementById('paybill-box').addEventListener('click',function(){
+//     document.getElementById('cashout').style.display = "none";
+//     document.getElementById('addmoney').style.display = 'none';
+//     document.getElementById('paybill').style.document = 'block';
+// })
