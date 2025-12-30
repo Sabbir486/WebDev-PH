9ff99d6ed8b1a868584d74fef0bfb58f216a7a00
Sabbir486 <australiasabbir355@gmail.com>
2025-12-31 02:49:29 +0600

    Add

--- Milestone- 5/03_Third_Module/Project/utilities/togglle.js
+++ Milestone- 5/03_Third_Module/Project/utilities/togglle.js
@@ -1,22 +1,27 @@
 document.getElementById('cashout').style.display = "none"; //by-default
 document.getElementById('paybill').style.display = "none"; //by-default
+document.getElementById('transaction-history').style.display = "none"; //by-default
+
 
 document.getElementById('add-money-box').addEventListener('click', function(){
     handleToggle('addmoney', 'block');
     handleToggle('cashout', 'none');
     handleToggle('paybill', 'none');
+    handleToggle("transaction-history", "none");
 })
 
 document.getElementById('cash-out-box').addEventListener('click', function(){
     handleToggle('addmoney', 'none');
     handleToggle('cashout', 'block');
     handleToggle('paybill', 'none');
+    handleToggle("transaction-history", "none");
 })
 
 document.getElementById('paybill-box').addEventListener('click', function(){
     handleToggle('addmoney', 'none');
     handleToggle('cashout', 'none');
     handleToggle('paybill', 'block');
+    handleToggle("transaction-history", "none");
 })
 
 
