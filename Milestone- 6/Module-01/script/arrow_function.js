5b38974f10a66e0c3b3cb505daa2bf2a7df6dd82
Sabbir486 <australiasabbir355@gmail.com>
2026-01-03 02:33:15 +0600

    Arrow Function

---
+++ Milestone- 6/Module-01/script/arrow_function.js
@@ -0,0 +1,18 @@
+// Normal Function
+
+function s(n1, n2){
+    return n1 + n2;
+}
+
+const result = s(100, 200);
+console.log(result);
+
+
+// Arrow Function
+
+const sum2 = (v1, v2) => {
+    return v1 + v2;
+}
+
+console.log(sum2(15,25));
+
