c06c07e414f564f77834945375215388e59fa4d8
Sabbir486 <australiasabbir355@gmail.com>
2026-01-03 02:21:09 +0600

    Template String

---
+++ Milestone- 6/Module-01/script/string.js
@@ -0,0 +1,14 @@
+//Template String
+
+const anthem = `I love Web Dev
+I love Australia.
+I love all`
+
+console.log(anthem);
+
+const country = "Bangladesh";
+
+const love = `I love ${country}. I love all people`;
+
+console.log(love);
+
