39add334a424ae7ab334a63330b1473e4d7c8403
Sabbir486 <australiasabbir355@gmail.com>
2026-01-03 03:20:33 +0600

    Arrow Function

--- Milestone- 6/Module-01/script/arrow_function.js
+++ Milestone- 6/Module-01/script/arrow_function.js
@@ -8,11 +8,25 @@ const result = s(100, 200);
 console.log(result);
 
 
-// Arrow Function
+// Arrow Function (Multiple Parameter)
 
 const sum2 = (v1, v2) => {
     return v1 + v2;
 }
 
 console.log(sum2(15,25));
+
+
+// Without Parameter
+
+const test = () => console.log("Hello Empty");
+test();
+
+// With One Parameter
+
+const test2 = a => a*a;
+console.log(test2(10));
+
+
+
 
