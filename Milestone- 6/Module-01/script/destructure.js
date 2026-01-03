18af20d77568004d7d1184a43c418e53104d7556
Sabbir486 <australiasabbir355@gmail.com>
2026-01-04 03:09:28 +0600

    Destructuring

---
+++ Milestone- 6/Module-01/script/destructure.js
@@ -0,0 +1,33 @@
+// Object Call
+
+const hena = {
+    name: 'Hena',
+    Age: 20,
+    status: "Not Found"
+}
+
+// Destructuring on Object
+
+const {name, Age, status} = hena;
+
+console.log(name);
+console.log(Age);
+
+
+
+// Destructuring on Array
+
+const person = ['haaa', 'raaa', 'taaa', {name:'sab'}];
+
+const [actor, coder, developer, obj] = person;
+
+console.log(coder);
+console.log(actor);
+console.log(developer);
+console.log(obj);
+
+
+
+
+
+
