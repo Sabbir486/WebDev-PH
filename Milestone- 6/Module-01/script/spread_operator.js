8e9b76009005b9e10691cc9e90d9a0d4d6027f77
Sabbir486 <australiasabbir355@gmail.com>
2026-01-04 02:52:00 +0600

    Spread Operator

---
+++ Milestone- 6/Module-01/script/spread_operator.js
@@ -0,0 +1,14 @@
+const friends = ['sa', 'ra', 'ta'];
+console.log(...friends); //... -> spread operator
+
+
+// Case-01
+const newArr = ['Hero', ...friends];
+console.log(newArr);
+
+// Case-02
+const numbers = [1,5,3,7,8,2];
+console.log(Math.max(...numbers));
+
+
+
