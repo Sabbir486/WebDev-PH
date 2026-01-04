9fb6a9dc6f2b54ba0b297b1d726705e99ad02a3a
Sabbir486 <australiasabbir355@gmail.com>
2026-01-05 03:39:47 +0600

    Map

---
+++ Milestone- 6/Module-02/javascript/map.js
@@ -0,0 +1,41 @@
+// Map --> Loop --> Always Return an Arrow
+
+const numbers = [1,2,3,4,5,6,7];
+
+// let temp = [];
+// for(let i=0; i<numbers.length; i++){
+//     const ele = numbers[i];
+//     const sum = ele + 1;
+//     temp.push(sum);
+// }
+
+// console.log(temp);
+
+
+// Case-01
+
+const newArr = numbers.map(ele => {
+    return ele+1;
+});
+
+console.log(newArr);
+
+
+// Case-02
+
+const square = numbers.map(ele => ele*ele);
+
+console.log(square);
+
+// Case-03
+
+const friends = ['ra', 'ka', 'ha', 'ta'];
+
+const newFriends = friends.map((element, index) => {
+    return index,element;
+});
+
+console.log(newFriends);
+
+
+
