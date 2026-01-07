e87bc2a9250a6cacaf6326e9c60d685ca78ceb05
Sabbir486 <australiasabbir355@gmail.com>
2026-01-08 02:12:26 +0600

    Undefined

---
+++ Milestone- 6/Module-03/script/undefined.js
@@ -0,0 +1,21 @@
+// console.log((names));
+
+// let name;
+// console.log(name);
+
+// let name = undefined;
+// console.log(name);
+
+
+// function sum(num1, num2){
+//     return (num1, num2);
+// }
+
+// console.log(sum());
+
+function sum(num1, num2){
+    return num1+num2;
+}
+
+const result = sum(10,5);
+console.log(result);
