d734ea32f6531bc13f831775f89aa67cca2d9daf
Sabbir486 <australiasabbir355@gmail.com>
2026-01-17 22:31:13 +0600

    Fetch

---
+++ Milestone- 6/Module-04/script/02_fetch.js
@@ -0,0 +1,10 @@
+// Fetch
+
+const handleLoadData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data);
+        
+    })
+}
