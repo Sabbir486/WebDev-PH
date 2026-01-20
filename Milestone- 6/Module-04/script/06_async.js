35b2b772271648b1a0e314235c94e5c76b1bb468
Sabbir486 <australiasabbir355@gmail.com>
2026-01-21 03:26:10 +0600

    Async Await

---
+++ Milestone- 6/Module-04/script/06_async.js
@@ -0,0 +1,33 @@
+//  const handleUser = () => {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data);
+        
+//     })
+
+//     .catch(error => console.log("Catch Error Successfully", error))
+//  }
+
+//  handleUser();
+
+
+const num1 = 10;
+const num2 = 20;
+
+
+//async function
+const handleUser2 = async() => {
+    const response =await fetch('https://jsonplaceholder.typicode.com/users')
+
+    const data = await response.json();
+    console.log(data);
+    
+}
+
+handleUser2();
+
+const result = num1+num2;
+console.log(result);
+console.log('Hello ..!!');
+
