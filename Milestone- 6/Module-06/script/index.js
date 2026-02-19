f337e63b8468f7d2096f8977d47d0b8c97908260
Sabbir486 <australiasabbir355@gmail.com>
2026-02-20 02:51:54 +0600

    Add

--- Milestone- 6/Module-06/script/index.js
+++ Milestone- 6/Module-06/script/index.js
@@ -6,11 +6,31 @@ function loadCategories(){
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
     .then((res) => res.json() )
         .then((data) => displayCategories(data.categories))
+
+        // data.categories --> Array of Object
     
 }
 
 function displayCategories(categories){
-    console.log(categories);
+    // get the container
+    const categoryContainer = document.getElementById('category-container');
+
+
+    // Loop Operation on Array of Object
+    for(let cat of categories){
+        console.log(cat);
+        
+        // CREATE Element
+        const categoryDiv = document.createElement('div');
+        categoryDiv.innerHTML = `
+        <button class="btn btn-sm hover:bg-[#ff1f3d] hover:text-white">${cat.category}</button>
+        `;
+
+        // Append Element
+        categoryContainer.append(categoryDiv);
+    }
+
+    
     
 }
 
