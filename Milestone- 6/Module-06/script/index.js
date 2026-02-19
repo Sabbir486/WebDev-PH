9da452b5393337bab7b7341036ff451c689403ad
Sabbir486 <australiasabbir355@gmail.com>
2026-02-19 15:56:38 +0600

    Add

---
+++ Milestone- 6/Module-06/script/index.js
@@ -0,0 +1,17 @@
+console.log("Index is connected");
+
+function loadCategories(){
+    //fetch the Data
+
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then((res) => res.json() )
+        .then((data) => displayCategories(data.categories))
+    
+}
+
+function displayCategories(categories){
+    console.log(categories);
+    
+}
+
+loadCategories();
