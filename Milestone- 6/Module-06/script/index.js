85438e09a42a8d9227fe5fb2a3921c00ed441970
Sabbir486 <australiasabbir355@gmail.com>
2026-02-20 03:47:04 +0600

    Add

--- Milestone- 6/Module-06/script/index.js
+++ Milestone- 6/Module-06/script/index.js
@@ -11,6 +11,13 @@ function loadCategories(){
     
 }
 
+function loadVideos(){
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then ((res) => res.json())
+    .then((data) => displayVideos(data.videos)
+    )
+}
+
 function displayCategories(categories){
     // get the container
     const categoryContainer = document.getElementById('category-container');
@@ -32,4 +39,34 @@ function displayCategories(categories){
 
 }
 
+const displayVideos=(videos) => {
+    
+    const videoContainer = document.getElementById('video-container');
+
+    for(let vid of videos){
+
+        const videoCard = document.createElement('div');
+        videoCard.innerHTML = `
+        <div class="card bg-base-100 shadow-sm">
+        <figure>
+            <img
+            src="${vid.thumbnail}"
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${vid.title}</h2>
+            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        </div>
+        `;
+
+        videoContainer.append(videoCard);
+    }
+    
+}
+
 loadCategories();
+loadVideos();
